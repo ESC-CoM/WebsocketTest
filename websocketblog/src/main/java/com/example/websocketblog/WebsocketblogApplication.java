8415e6644d3bb1e8e9e552d5d19a00cbfee0bb98
dsjinwongo <dsgo22jw@chungbuk.ac.kr>
2022-04-12 17:13:23 +0900

    Feat: Initial commit from websocketblog

---
+++ websocketblog/src/main/java/com/example/websocketblog/WebsocketblogApplication.java
@@ -0,0 +1,13 @@
+package com.example.websocketblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocketblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsocketblogApplication.class, args);
+	}
+
+}
